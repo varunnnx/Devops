3f3338382765b90cd946f9049ce87be1108c2b9f
varunnnx <113424492+varunnnx@users.noreply.github.com>
2024-08-29 12:57:00 +0530

    Update SampleProgram1.java

--- SampleProgram1.java
+++ SampleProgram1.java
@@ -1,5 +1,5 @@
-﻿public class SampleProgram1 {
-    public static void main(String[] args) {
+﻿public class SampleProgram1{
+    public static void main(String[] args){
         System.out.println("Hello, World!");
     }
 }
